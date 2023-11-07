523395fbee11e23a16288248f21595569f728c98
roksana1818 <110596355+roksana1818@users.noreply.github.com>
2023-11-07 23:44:03 +0200

    Update deps.js
    fix bug

--- deps.js
+++ deps.js
@@ -1,7 +1,1 @@
-import { serve } from "./deps.js";
-
-const handleRequest = (request) => {
-  return new Response("Seeking truths beyond meaning of life, you will find 43.");
-};
-
-serve(handleRequest, { port: 7777 });
+export { serve } from "https://deno.land/std@0.202.0/http/server.ts";
